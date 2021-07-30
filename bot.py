063f700512e0f9b67449484b6a6a93496625e828
LUSIFER <70184956+lusifernoob@users.noreply.github.com>
2021-07-30 14:59:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ async def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "@Infinity_Bots"
+        rep = "@OMG_info"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
